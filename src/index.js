0d33c62b50ffa38d410c77af880701372a69b1b3
Joseph R Miles <joe@jrm.dev>
2021-06-05 07:43:09 -0700

    chore: oops

--- src/index.js
+++ src/index.js
@@ -1,29 +1,34 @@
 const express = require("express");
-const fs = require("fs").promises;
+const fs = require("fs");
 
-const main = async () =>
+const hookd = express();
+
+const scriptsDir = fs.opendirSync("/tmp/scripts");
+console.log(scriptsDir);
+
+const registerScripts = (dir, app) =>
 {
-  const hookd = express();
-
-  hookd.get("/up", (_, response) => response.sendStatus(200));
-
-  hookd.post(
-    "/docker",
-    (request, response) =>
-    {
-      console.log(request);
-      response.send();
-    }
-  );
-
-  const PORT = process.env.HOOKD_PORT || 8080;
-  hookd.listen(
-    PORT,
-    () =>
-    {
-      console.log(`Running on port ${PORT}.`);
-    }
-  );
+  // TODO
 };
 
-main();
+hookd.get("/up", (_, response) => response.sendStatus(200));
+
+hookd.post(
+  "/docker",
+  (request, response) =>
+  {
+    console.log(request);
+    response.send();
+  }
+);
+
+const PORT = process.env.HOOKD_PORT || 8080;
+hookd.listen(
+  PORT,
+  () =>
+  {
+    console.log(`Running on port ${PORT}.`);
+  }
+);
+
+process.on("exit", () => root.close());
