441b1ae506d11cd89b4b85e9c8c3bfec3345371c
Joseph R Miles <joe@jrm.dev>
2021-06-04 20:46:38 -0700

    improvement: wrapped code in main function

--- src/index.js
+++ src/index.js
@@ -1,24 +1,29 @@
 const express = require("express");
 const fs = require("fs").promises;
 
-const hookd = express();
+const main = async () =>
+{
+  const hookd = express();
 
-hookd.get("/up", (_, response) => response.sendStatus(200));
+  hookd.get("/up", (_, response) => response.sendStatus(200));
 
-hookd.post(
-  "/docker",
-  (request, response) =>
-  {
-    console.log(request);
-    response.send();
-  }
-);
+  hookd.post(
+    "/docker",
+    (request, response) =>
+    {
+      console.log(request);
+      response.send();
+    }
+  );
 
-const PORT = process.env.HOOKD_PORT || 8080;
-hookd.listen(
-  PORT,
-  () =>
-  {
-    console.log(`Running on port ${PORT}.`);
-  }
-);
+  const PORT = process.env.HOOKD_PORT || 8080;
+  hookd.listen(
+    PORT,
+    () =>
+    {
+      console.log(`Running on port ${PORT}.`);
+    }
+  );
+};
+
+main();
